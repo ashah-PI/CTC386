53464ad3ec4a5dbeea3d4c2d70a999dc25cf3dad
Arpita <ashah16@csudh.edu>
2026-01-29 01:41:05 +0000

    Final Exam example 4

---
+++ Finalexam4.py
@@ -0,0 +1,10 @@
+# Arpita Shah
+# Final Exam Ex 4
+
+#Define function
+def happy():
+  for i in range (number):
+   print("Happy New Year!")
+
+number = int(input("Enter a number between 1 to 10:  "))
+happy()
